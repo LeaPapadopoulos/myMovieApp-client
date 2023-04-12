b925044017608766a3454e35c0363f4d3567ca73
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2023-04-12 22:24:55 +0200

    add bootstrap styles to movie card and view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,20 @@
+import React from "react";
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
+// import "./movie-card.scss";
+
 export const MovieCard = ({ movie, onBookClick }) => {
   return (
-    <div
-      onClick={() => {
-        onBookClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director}</Card.Text>
+        <Button onClick={() => onBookClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 MovieCard.propTypes = {
